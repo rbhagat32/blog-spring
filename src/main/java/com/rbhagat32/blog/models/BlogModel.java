fae74cfdd962f75e5a9969b19d87917f190d27db
rbhagat32 <raghavbhagat32@gmail.com>
2025-09-07 15:57:41 +0530

    user model + auto-index-creation

--- src/main/java/com/rbhagat32/blog/models/BlogModel.java
+++ src/main/java/com/rbhagat32/blog/models/BlogModel.java
@@ -1,8 +1,7 @@
 package com.rbhagat32.blog.models;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,13 +9,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
 
 @Document(collection="blogs")
-@Getter
-@Setter
-//@Data -> includes Getter, Setter, Required Args Constructor, ToString, EqualsAndHashCode, Value
+@Data
 public class BlogModel {
     @Id
     private ObjectId id;
+
+    @NonNull
     private String title;
+
     private String content;
+
     private Date createdAt;
 }
